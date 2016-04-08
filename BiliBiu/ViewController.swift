e089c953950008e6f5ad2a16f1dceaea6d8f19a5
catna <catnaer@163.com>
2016-04-08 15:58:48 +0800

    initial repository

---
+++ BiliBiu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BiliBiu
+//
+//  Created by MX on 16/4/8.
+//  Copyright © 2016年 MX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.cyanColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
