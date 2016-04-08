6d7b7d5065097d60e859ada1986ec7767766a3b9
catna <catnaer@163.com>
2016-04-08 16:14:19 +0800

    CHANGE:info.plist allow http protocol key

--- BiliBiu/ViewController.swift
+++ BiliBiu/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var avPlayer: AVPlayer = AVPlayer.init(URL: NSURL.init(string: "http://cn-shcy5-dx.acgvideo.com/vg2/e/88/6852792hd.mp4?expires=1460115000&ssig=v0H6zluiRLPLPAOgIsIX-Q&oi=3031252344&appkey=86385cdc024c0f6c&or=3026306825&rate=0")!)
+    var avLayer: AVPlayerLayer = AVPlayerLayer.init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.cyanColor()
+        avLayer.player = avPlayer
+        avLayer.frame = self.view.bounds
+        self.view.layer.addSublayer(avLayer)
     }
 
     override func didReceiveMemoryWarning() {
