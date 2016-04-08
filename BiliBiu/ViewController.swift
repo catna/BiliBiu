30c50fd20833ea43ce553ca898e2cbe79834d9db
catna <catnaer@163.com>
2016-04-08 17:05:51 +0800

    Test:AVFoundation function

--- BiliBiu/ViewController.swift
+++ BiliBiu/ViewController.swift
@@ -11,7 +11,12 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
-    var avPlayer: AVPlayer = AVPlayer.init(URL: NSURL.init(string: "http://cn-shcy5-dx.acgvideo.com/vg2/e/88/6852792hd.mp4?expires=1460115000&ssig=v0H6zluiRLPLPAOgIsIX-Q&oi=3031252344&appkey=86385cdc024c0f6c&or=3026306825&rate=0")!)
+    lazy var avPlayer: AVPlayer = {
+        let apItem = AVPlayerItem.init(URL: NSURL.init(string: "http://cn-shcy5-dx.acgvideo.com/vg2/e/88/6852792hd.mp4?expires=1460115000&ssig=v0H6zluiRLPLPAOgIsIX-Q&oi=3031252344&appkey=86385cdc024c0f6c&or=3026306825&rate=0")!)
+        let ap = AVPlayer.init(playerItem: apItem)
+        return ap
+    }()
+    
     var avLayer: AVPlayerLayer = AVPlayerLayer.init()
     
     override func viewDidLoad() {
@@ -20,6 +25,11 @@ class ViewController: UIViewController {
         avLayer.player = avPlayer
         avLayer.frame = self.view.bounds
         self.view.layer.addSublayer(avLayer)
+        self.avPlayer.play()
+        
+        let apItem2 = AVPlayerItem.init(URL: NSURL.init(string: "http://cn-shcy3-dx.acgvideo.com/vg8/8/88/6966263hd.mp4?expires=1460118900&ssig=M4UkZhM3rQtfGG4567OxDA&oi=3031252344&appkey=86385cdc024c0f6c&or=3026306825&rate=0")!)
+        self.avPlayer.rate = 2.0
+        self.avPlayer.replaceCurrentItemWithPlayerItem(apItem2)
     }
 
     override func didReceiveMemoryWarning() {
